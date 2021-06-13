103a16c6b1cdf5c76623a2e5e8103508c883b50f
7Magic7Mike7 <michael.artner@hotmail.de>
2021-06-13 18:48:09 +0200

    MapWidget renders map

--- widgets/map_widget.py
+++ widgets/map_widget.py
@@ -1,6 +1,7 @@
 
 from py_cui.widgets import BlockLabel
 from util.logger import Logger
+from game.map.map import Map
 
 
 class MapWidget:
@@ -11,3 +12,27 @@ class MapWidget:
 
     def move(self, direction):
         self.__logger.print(f"Moving in direction {direction}")
+
+    def set_map(self, map: Map):
+        self.__map = map
+
+    def render(self):
+        str_repr = map_to_string(self.__map)
+        self.__map_widget.set_title(str_repr)
+
+
+def map_to_string(map: Map):
+    repr = ""
+    for y in range(map.height()):
+        for x in range(map.width()):
+            val = map.at(x, y)
+            if val == 1:
+                repr += "_"
+            elif val == 2:
+                repr += "#"
+            elif val == 3:
+                repr += "~"
+            else:
+                repr += " "
+        repr += '\n'    # todo change to new line from system?
+    return repr
