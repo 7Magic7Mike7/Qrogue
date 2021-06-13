91e7c2e1ef8dbd7f58c87404078b6b8a293e47e0
7Magic7Mike7 <michael.artner@hotmail.de>
2021-06-14 00:32:07 +0200

    added direction enum

--- game/map/navigation.py
+++ game/map/navigation.py
@@ -1,10 +1,55 @@
 
+from enum import Enum
+
+
 class Coordinate:
     def __init__(self, x: int, y: int):
         self.__x = x
         self.__y = y
 
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
     def resolve(self):
         return self.__x, self.__y
 
-#enum Direction:
+
+class Direction(Enum):
+    North = (0, -1)
+    East = (1, 0)
+    South = (0, 1)
+    West = (-1, 0)
+    N = North
+    E = East
+    S = South
+    W = West
+
+    Up = North
+    Right = East
+    Down = South
+    Left = West
+    U = Up
+    R = Right
+    D = Down
+    L = Left
+
+    def __init__(self, x: int, y: int):
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    # @property
+    # def c(self):
+    #    return Coordinate(self.__x, self.__y)
