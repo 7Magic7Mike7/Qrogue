07f54ab48753581ce3363cea5976c947ffac27e9
7Magic7Mike7 <michael.artner@hotmail.de>
2021-06-15 09:45:18 +0200

    implemented Tile-enum

--- game/map/map.py
+++ game/map/map.py
@@ -1,6 +1,7 @@
 
 import random
-from game.map.navigation import *
+from enum import Enum
+from game.map.navigation import Coordinate, Direction
 
 P = 0   # player
 A = 1
@@ -8,24 +9,51 @@ F = 7
 E = 100 # empty field
 
 
+class Tile(Enum):
+    Empty = (0, " ", True)
+    Player = (1, "P")
+    Enemy = (10, "E")
+    Wall = (100, "#")
+    Obstacle = (110, "o")
+
+    def __init__(self, code: int, img: str, walkable: bool = False):
+        self.__code = code
+        self.__img = img[0]
+        self.__walkable = walkable
+
+    @property
+    def code(self):
+        return self.__code
+
+    @property
+    def img(self):
+        return self.__img
+
+    def is_walkable(self):
+        return self.__walkable
+
+
 class Map:
     def __init__(self, seed: int, width: int, height: int):
         rand = random.Random()
         rand.seed(seed)
 
-        self.__player_pos = Coordinate(1, 1)
+        self.__player_pos = Coordinate(2, 2)
 
         self.__map = []
         for y in range(height):
             row = []
             for x in range(width):
                 if y == 0 or y == height - 1 or x == 0 or x == width - 1:
-                    row.append('#')
+                    row.append(Tile.Wall)
                 else:
-                    row.append(rand.randint(A, F))
+                    if rand.randint(0, 7) == 1:
+                        row.append(Tile.Obstacle)
+                    else:
+                        row.append(Tile.Empty)
             self.__map.append(row)
 
-        self.__map[1][1] = P
+        self.__map[self.__player_pos.y][self.__player_pos.x] = Tile.Player
 
     def height(self):
         return len(self.__map)
@@ -50,7 +78,10 @@ class Map:
                 new_pos.x < 0 or len(self.__map[0]) <= new_pos.x:
             return False
 
-        self.__player_pos = new_pos
-        self.__map[y][x] = E
-        self.__map[new_pos.y][new_pos.x] = P
-        return True
+        if self.__map[new_pos.y][new_pos.x].is_walkable():
+            self.__player_pos = new_pos
+            self.__map[y][x] = Tile.Empty
+            self.__map[new_pos.y][new_pos.x] = Tile.Player
+            return True
+        else:
+            return False
