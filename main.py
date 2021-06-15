ac75d27f1595203f79a8947a69a4c78662112a78
7Magic7Mike7 <michael.artner@hotmail.de>
2021-06-15 23:03:10 +0200

    using the new GameHandler

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #D:\Programs\anaconda3\envs\Qrogue
 # This is a sample Python script.
+from game.game import GameHandler
 
-from widgets.qrogue_pycui import QroguePyCUI
+note = """
+Climate Crisis Narrative? E.g. the game plays on earth in 2070, most places have been destroyed 
+and the remaining lives are weird mutations/... 
+The goal is to find parts of a Quantum Computer (inspiration from Entanglion) so we are able to 
+turn back time so we can "stop" the climate crisis and live on a healthy planet?
+"""
 
-root = QroguePyCUI()
-root.start()
+game = GameHandler(7)
+game.log(note)
+game.start()
