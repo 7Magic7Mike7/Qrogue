beb01ab6bf53b1f63e6f51fecc15374413173d8c
7Magic7Mike7 <michael.artner@hotmail.de>
2021-06-17 22:03:33 +0200

    added selection controls

--- game/controls.py
+++ game/controls.py
@@ -9,19 +9,45 @@ class Controls:
         self.__move_down = KEY_DOWN_ARROW
         self.__move_left = KEY_LEFT_ARROW
 
+        self.__selection_up = KEY_UP_ARROW
+        self.__selection_right = KEY_RIGHT_ARROW
+        self.__selection_down = KEY_DOWN_ARROW
+        self.__selection_left = KEY_LEFT_ARROW
+
         self.__render = KEY_R_LOWER
 
+    @property
     def move_up(self):
         return self.__move_up
 
+    @property
     def move_right(self):
         return self.__move_right
 
+    @property
     def move_down(self):
         return self.__move_down
 
+    @property
     def move_left(self):
         return self.__move_left
+
+    @property
+    def selection_up(self):
+        return self.__selection_up
+
+    @property
+    def selection_right(self):
+        return self.__selection_right
+
+    @property
+    def selection_down(self):
+        return self.__selection_down
+
+    @property
+    def selection_left(self):
+        return self.__selection_left
+
 
     def render(self):
         return self.__render
