8284d259ab436a6c92b35f3ad852a63cb2caf440
7Magic7Mike7 <michael.artner@hotmail.de>
2021-06-24 02:10:00 +0200

    wrote own renderer for the circuits

--- widgets/circuit_widget.py
+++ widgets/circuit_widget.py
@@ -17,4 +17,23 @@ class CircuitWidget:
 
     def render(self):
         if self.__player is not None:
-            self.__widget.set_title(self.__player.circuit.__str__())
+            circ_str = ""       # todo prepend generator!
+            instructions = self.__player.instructions
+            if len(instructions) > 0:
+                rows = [""] * self.__player.num_of_qubits
+                for inst in instructions:
+                    used_qubits = [False] * self.__player.num_of_qubits
+                    max_len = 0
+                    for q in inst.qargs:
+                        used_qubits[q] = True
+                        inst_str = f"--|{inst.abbreviation(q)}|--"
+                        max_len = max(max_len, len(inst_str))
+                        rows[q] += inst_str
+                    for i in range(len(used_qubits)):
+                        if not used_qubits[i]:
+                            rows[i] += ("-" * max_len)
+            else:
+                rows = ["-------"] * self.__player.num_of_qubits
+            for row in rows:
+                circ_str += row + "\n"
+            self.__widget.set_title(circ_str)
