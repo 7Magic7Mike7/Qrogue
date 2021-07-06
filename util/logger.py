479459af8f9a943a5e0ec402f66813fbc168d1d5
7Magic7Mike7 <michael.artner@hotmail.de>
2021-07-06 22:07:44 +0200

    added print_list

--- util/logger.py
+++ util/logger.py
@@ -14,6 +14,14 @@ class Logger:
     def println(self, message=""):
         self.print(message + "\n")  # TODO use system dependent new line?
 
+    def print_list(self, list=[], delimiter: str = ", "):
+        sb = ""
+        for elem in list:
+            if elem is not None:
+                sb += elem.__str__()
+            sb += delimiter
+        return sb
+
     def clear(self):
         self.__text = ""
         self.print("")
