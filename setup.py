aa6aba0b8bf455d7e7e77a46af1cb41d77ec002b
7Magic7Mike7 <michael.artner@hotmail.de>
2022-03-15 14:23:56 +0100

    updated setup for PyPI upload

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ setup(
   name = 'Qrogue',         # Name of package folder
   packages = ['Qrogue'],   # Same as "name"
   version = '0.2.1',
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository  # todo
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Qrogue is a modernized Quantum Computing take of the classical game Rogue. It can be played in a '
                 'terminal and aims to improve your understanding of Quantum Computing',
   author = 'Michael Artner',
   author_email = 'michael.artner@jku.at',
   url = 'https://github.com/7Magic7Mike7/Qrogue',
-  download_url = '',    # link to Release on GitHub # todo
+  download_url = 'https://github.com/7Magic7Mike7/Qrogue/releases/tag/0.2.1',    # link to Release on GitHub
   keywords = ['Quantum Computing', 'Gamification', 'Video Game', 'PyCUI'],
   install_requires=[
           'py-cui',
@@ -20,7 +20,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     'Intended Audience :: Anyone interested in Quantum Computing',
     'Topic :: Quantum Computing :: Gamification',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license # todo
+    'License :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3.8',
   ],
 )
