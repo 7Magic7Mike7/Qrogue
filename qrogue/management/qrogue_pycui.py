6bb8a8fbdf87ab23c8fb935d51596a878620876d
7Magic7Mike7 <michael.artner@hotmail.de>
2022-06-22 16:17:25 +0200

    removed the hard-coded introduction message since levels can have their own

--- qrogue/management/qrogue_pycui.py
+++ qrogue/management/qrogue_pycui.py
@@ -478,8 +478,6 @@ class QrogueCUI(PyCUI):
             # load the newest level (exam phase) by
             MapManager.instance().load_first_uncleared_map()
             self.__state_machine.change_state(State.Explore, None)
-            if not Ach.completed_exam_phase1(progress):
-                Popup.examiner_says(HelpText.get(HelpTextType.FirstLevelIntroduction))
 
     def switch_to_spaceship(self, data=None):
         StoryNarration.returned_to_spaceship()
