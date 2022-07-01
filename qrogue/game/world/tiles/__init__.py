e96a01ddc9c10e10701419ee9a61541041394f15
7Magic7Mike7 <michael.artner@hotmail.de>
2022-07-01 22:09:50 +0200

    added new Challenger Tile

--- qrogue/game/world/tiles/__init__.py
+++ qrogue/game/world/tiles/__init__.py
@@ -2,7 +2,7 @@
 from .tiles import TileCode, Tile
 from .tiles import Invalid, Debug, Void, Floor, Wall, Obstacle, FogOfWar, Decoration, ControllableTile
 from .walk_trigger_tiles import WalkTriggerTile, Trigger, Teleport, Tunnel, Message, Riddler, ShopKeeper, Collectible, \
-    Energy
+    Energy, Challenger
 from .puzzle_tiles import Enemy, Boss
 from .door_tiles import DoorOpenState, DoorOneWayState, DoorEntanglementState, Door, HallwayEntrance
 
