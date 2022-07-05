2c1cc5a610edb14ac4d702d8e7aa072264e59d60
7Magic7Mike7 <michael.artner@hotmail.de>
2022-07-05 13:39:08 +0200

    updated method name (testing overhaul is for next release)

--- qrogue/test/save_load_test.py
+++ qrogue/test/save_load_test.py
@@ -11,7 +11,7 @@ print(sv.achievement_manager.to_string())
 
 sv.achievement_manager.finished_level("l1v1")
 sv.achievement_manager.add_to_achievement(achievements.CompletedExpedition, 1)
-sv.achievement_manager.finished_tutorial(achievements.FinishedTutorial)
+sv.achievement_manager.progressed_in_story(achievements.FinishedTutorial)
 print(sv.achievement_manager.to_string())
 
 sv.save()
