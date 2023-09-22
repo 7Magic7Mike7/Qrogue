b23b4ba2298c5fffb0c600c1ddbf9586038f045e
Michael Artner <michael.artner@hotmail.de>
2023-09-22 13:10:48 +0200

    updated help texts and added one for ket-notation, parallel gates and serial gates

--- qrogue/util/help_texts.py
+++ qrogue/util/help_texts.py
@@ -16,7 +16,11 @@ class _HL:
     enemies = CC.highlight_object("Enemies")
     energy = CC.highlight_object("Energy")
     gates = CC.highlight_object("Gates")
+    gate_i = CC.highlight_object("I Gate")
+    gate_cx = CC.highlight_object("CX Gate")
+    gate_x = CC.highlight_object("X Gate")
     input_stv = CC.highlight_object("Input State Vector")
+    ket_notation = CC.highlight_object("Ket-Notation")
     key = CC.highlight_object("Key")
     keys = CC.highlight_object("Keys")
 
@@ -39,24 +43,34 @@ class _HL:
     target_stv = CC.highlight_object("Target State Vector")
 
     # actions
+    applying = CC.highlight_action("applying")
     buy = CC.highlight_action("buy")
     change = CC.highlight_action("change")
     choose = CC.highlight_action("choose")
     editing = CC.highlight_action("editing")
+    edit = CC.highlight_action("edit")
     edits = CC.highlight_action("edits")
     position = CC.highlight_action("position")
     place = CC.highlight_action("place")
+    reach = CC.highlight_action("reach")
+    select = CC.highlight_action("select")
     talk = CC.highlight_action("talk")
+    try_ = CC.highlight_action("try")
 
     # words
+    amplitude_1 = CC.highlight_word("amplitude of 1")
+    arbitrary = CC.highlight_word("arbitrary")
+    around = CC.highlight_word("around")
     attempt = CC.highlight_word("Attempt")
     bell = CC.highlight_word("Bell, the Master of Entanglement")
     both0 = CC.highlight_word("both 0")
     both1 = CC.highlight_word("both 1")
 
+    cannot = CC.highlight_word("cannot")
     chance_based = CC.highlight_word("chance based")
     continue_ = CC.highlight_word("Continue")
     details = CC.highlight_word("details")
+    deterministic = CC.highlight_word("deterministic")
     difference = CC.highlight_word("Difference")
     display_details = CC.highlight_word("displays details")
     easier = CC.highlight_word("easier")
@@ -70,6 +84,7 @@ class _HL:
     finally_ = CC.highlight_word("finally")
     five_parenthesis = CC.highlight_word("5)")
     flee = CC.highlight_word("Flee")
+    four = CC.highlight_word("four")
     four_parenthesis = CC.highlight_word("4)")
 
     gate = CC.highlight_object("Gate")
@@ -77,6 +92,10 @@ class _HL:
     help_ = CC.highlight_word("Manual")
     hp = CC.highlight_word("don't lose Energy")
     items = CC.highlight_word("Items")
+    ket1_symbol = CC.highlight_word("|?>")
+    ket2_symbol = CC.highlight_word("|??>")
+    ket_symbol_stv01 = CC.highlight_word("|0> 1")
+    kronecker = CC.highlight_word("kronecker product")
     leave = CC.highlight_word("\"-Leave-\"")
     list_ = CC.highlight_word("list")
     location = CC.highlight_word("Location")
@@ -84,32 +103,47 @@ class _HL:
     loses_energy = CC.highlight_word("loses some Energy")
     map_ = CC.highlight_word("map")
     manual = CC.highlight_word("Manual")
-    mat_vec_mul = CC.highlight_word("Matrix Vector Multiplication")
+    matrix_2x2 = CC.highlight_word("2x2 matrix")
+    matrix_4x4 = CC.highlight_word("4x4 matrix")
+    mat_vec_mul = CC.highlight_word("matrix-vector multiplication")
     mission = CC.highlight_word("Mission")
     moon = CC.highlight_word("Moon")
     moon_mission = CC.highlight_word("Moon Mission")
     navigate = CC.highlight_word("navigate")
     navigation_panel = CC.highlight_word("Navigation Panel")
     not_zero = CC.highlight_word("not zero")
+    number_of_times = CC.highlight_word("number of times")
+    often = CC.highlight_word("often")
     one_hp = CC.highlight_word("1 HP")
     one_parenthesis = CC.highlight_word("1)")
     options = CC.highlight_word("Options")
+    order = CC.highlight_word("order")
+    probability = CC.highlight_word("probability")
 
     quantum_computing = CC.highlight_word("Quantum Computing")
     quantum_algorithm = CC.highlight_word("Quantum Algorithm")
+    qubit_configuration = CC.highlight_word("qubit configuration")
+    qubit_configurations = CC.highlight_word("qubit configurations")
     reenter = CC.highlight_word("re-enter")
     removed = CC.highlight_word("removed")
     reward = CC.highlight_word("Reward")
     serious = CC.highlight_word("serious")
     start_journey = CC.highlight_word("START YOUR JOURNEY")
+    state_vector = CC.highlight_word("StateVector")
+    state_vectors = CC.highlight_word("StateVectors")
     superposition = CC.highlight_word("Superposition")
-    two_parenthesis = CC.highlight_word("2)")
     three_parenthesis = CC.highlight_word("3)")
+    transforms = CC.highlight_word("transforms")
+    two = CC.highlight_word("two")
+    two_parenthesis = CC.highlight_word("2)")
+    unstable = CC.highlight_word("unstable")
     use = CC.highlight_word("use")
     vanishes = CC.highlight_word("vanishes")
+    vanish = CC.highlight_word("vanish")
     win = CC.highlight_word("win")
     zero_energy = CC.highlight_word("0 energy")
     zeros = CC.highlight_word("zeros")
+    zero_state = CC.highlight_word("zero-state")
 
     # keys
     action_keys = CC.highlight_key("Space") + " or " + CC.highlight_key("Enter")
@@ -149,21 +183,85 @@ class HelpText(Enum):
             "preferred option over simply closing the window!]"  # let's not mention ESC since it could lead to bugs
 
     Fight = \
-        f"Basically {_HL.quantum_computing} is just a lot of complex {_HL.mat_vec_mul}:\n" \
+        f"Basically {_HL.quantum_computing} is just a lot of complex-valued {_HL.mat_vec_mul}:\n" \
         f"{_HL.circuit_matrix} * {_HL.input_stv} = {_HL.output_stv}\n" \
-        f"Your goal is to make the latter {_HL.equal} to the {_HL.target_stv}. While the input state vector " \
-        f"always stays the same, you can {_HL.change} the {_HL.circuit_matrix} by {_HL.editing} your circuit.\n" \
-        f"First you have to {_HL.choose} and then {_HL.place} a {_HL.gate} (via {_HL.navigation_keys}). " \
+        f"Your goal is to make the latter {_HL.equal} to the {_HL.target_stv}. While the input and target state " \
+        f"vector are given by the {_HL.puzzle}, you can {_HL.change} the {_HL.circuit_matrix} by {_HL.editing} your " \
+        f"{_HL.circuit}.\n" \
+        f"First you have to {_HL.select} a {_HL.gate} and then {_HL.place} it via {_HL.navigation_keys}. " \
         f"Afterwards use {_HL.action_keys} to confirm the placement. This will update both {_HL.circuit_matrix} " \
         f"and {_HL.output_stv} accordingly. Green numbers in {_HL.output_stv} indicate correct and red incorrect " \
         f"values. Once the whole vector is green, {_HL.output_stv} and {_HL.target_stv} are equal and the " \
         f"{_HL.puzzle} is solved."
 
+    Ket = \
+        f"The {_HL.ket1_symbol}-notation you can see {_HL.around} the {_HL.state_vectors} and {_HL.circuit_matrix} " \
+        f"is called {_HL.ket_notation}. It describes to which {_HL.qubit_configuration} the values correspond to. For " \
+        f"example {_HL.ket_symbol_stv01} at ~Target~ means that the target qubit's {_HL.zero_state} should have an " \
+        f"{_HL.amplitude_1}, where the amplitude corresponds to a {_HL.probability} of how likely the described " \
+        f"{_HL.state} occurs. Let's consider the following (transposed) {_HL.state_vector}:\n" \
+        f"|00>  0.707\n" \
+        f"|01>  0.707\n" \
+        f"|10>    0  \n" \
+        f"|11>    0  \n" \
+        f"It implies that both {_HL.qubit_configurations} |00> and |01> have an amplitude of 0.707 and therefore " \
+        f"correspond to a probability of abs(0.707)^2 = 0.5 = 50%. Since we write the most significant qubit first, " \
+        f"this implies that q1=0 is always true and only q0's value comes down to a coin flip.\n" \
+        f"In general {_HL.ket_notation} looks like this: |qn...q1q0>, where n is the number of qubits - 1."
+
+    ParallelGates = \
+        f"Single qubit {_HL.gates} (e.g., X Gate) correspond to {_HL.matrix_2x2} which describe how the {_HL.gate} " \
+        f"{_HL.transforms} the two possible inputs (0 and 1). However, {_HL.circuit} with {_HL.two} {_HL.qubits} " \
+        f"have {_HL.four} possible inputs (00, 01, 10 and 11) and therefore have to correspond to {_HL.matrix_4x4}. " \
+        f"Hence, to extend the original 2x2 matrix of the X Gate we need to calculate a so-called {_HL.kronecker}.\n" \
+        f"\n" \
+        f"Let's consider a circuit with an {_HL.gate_x} applied to q0. What happens to q1?\n" \
+        f"Not performing an operation on it is the same as {_HL.applying} the identity or {_HL.gate_i} (transforming " \
+        f"0 to 0 and 1 to 1) to it. Now to calculate the {_HL.kronecker} of X (on q0) and I (on q1) we have to " \
+        f"{CC.highlight_word('put X into I')} like this:\n" \
+        "    |0>  |1>        |0> |1>\n" \
+        "|0> 1*X  0*X  =  |0> X   0 \n" \
+        "|1> 0*X  1*X     |1> 0   X \n" \
+        "\n" \
+        f"and furthermore as we {CC.highlight_word('unpack')} X:\n" \
+        "    |00> |01> |10> |11>\n" \
+        "|00>  0    1    0    0 \n" \
+        "|01>  1    0    0    0 \n" \
+        "|10>  0    0    0    1 \n" \
+        "|11>  0    0    1    0 \n" \
+        "\n" \
+        f"The order is important as {CC.highlight_word('putting X into I')} results in a different matrix than " \
+        f"{CC.highlight_word('putting I into X')}. But if you look at your {_HL.circuit} you can always go from " \
+        f"{CC.highlight_word('top to bottom')}. Just use the {CC.highlight_word('top most')} {_HL.gate} as base and " \
+        f"{CC.highlight_word('put the lower')} one(s) into it."
+
+    SerialGates = \
+        f"Applying two {_HL.gates} in series is as easy as {CC.highlight_word('multiplying')} two matrices.\n" \
+        f"Let's consider an {_HL.gate_x} at q0 followed by a {_HL.gate_cx} at q0 (=control), q1 (=target). So we " \
+        f"compute a {_HL.matrix_4x4} for the {_HL.gate_x} (see {_HL.kronecker} and then " \
+        f"{CC.highlight_word('multiply')} the matrix of the {_HL.gate_cx} {CC.highlight_word('from the left')}:\n" \
+        f"    |00> |01> |10> |11>         |00> |01> |10> |11>         |00> |01> |10> |11>\n" \
+        f"|00>  1    0    0    0      |00>  0    1    0    0      |00>  0    1    0    0 \n" \
+        f"|01>  0    0    0    1   *  |01>  1    0    0    0   =  |01>  0    0    1    0 \n" \
+        f"|10>  0    0    1    0      |10>  0    0    0    1      |10>  0    0    0    1 \n" \
+        f"|11>  0    1    0    0      |11>  0    0    1    0      |11>  1    0    0    0 \n" \
+        f"\n" \
+        f"If we take a look at the result's column {CC.highlight_word('|00>')} we can see that the resulting matrix " \
+        f"{_HL.transforms} this state to {CC.highlight_word('|11>')} as expected (i.e., X negates q0=0 to q0=1 and " \
+        f"then CX negates q1=0 to q1=1 since it's control (q0) is 1).\n" \
+        f"Again, in general the {_HL.order} of multiplication matters. At first glance it may seem counter-intuitive " \
+        f"that the {_HL.gates} are aligned from {CC.highlight_word('left to right')} in the {_HL.circuit} but have " \
+        f"to be {CC.highlight_word('multiplied')} from {CC.highlight_word('right to left')}. But in the " \
+        f"{_HL.mat_vec_mul} the {_HL.input_stv} is on the right side while for the {_HL.circuit} the input is on the " \
+        f"left. Therefore in both cases simply the gate/matrix {CC.highlight_word('closer')} to the input is " \
+        f"considered first."
+
     Riddle = \
-        f"{_HL.riddles} are very similar to {_HL.puzzles}: There is a {_HL.target_state} you need to reach " \
-        f"by adapting your {_HL.circuit}. The main difference is that there's a predefined maximum number of " \
-        f"{_HL.edits} for solving it. When you have no more {_HL.edit_attempts} left the Riddle {_HL.vanishes} " \
-        f"together with its {_HL.reward} - which is often much better than the ones from regular Puzzles."
+        f"{_HL.riddles} are very similar to {_HL.puzzles}, yet a bit different. They also shows you a " \
+        f"{_HL.target_state} you need to {_HL.reach} to solve it but you {_HL.cannot} {_HL.try_} it " \
+        f"{_HL.arbitrary} {_HL.often}. They have predefined {_HL.number_of_times} you can {_HL.edit} your " \
+        f"{_HL.circuit} before they get {_HL.unstable}. Unstable {_HL.riddles} have a 50% chance to {_HL.vanish} " \
+        f"together with their {_HL.reward} whenever you update your {_HL.circuit}."
 
     Shop = \
         f"In the {_HL.shop} you can exchange {_HL.coins} you got (e.g. from solving Puzzles) for various " \
@@ -193,7 +291,7 @@ class HelpText(Enum):
 
     Options = "7"
     Welcome = Game + \
-        "\nBut before you are allowed to explore the universe you have to complete a trainings program.\n" \
+        "\nBut before you can explore the universe you have to complete a trainings program.\n" \
         f"Now close this dialog by pressing {_HL.action_keys}. Select {_HL.start_journey} with " \
         f"{_HL.navigation_keys} and confirm your selection with {_HL.action_keys} to begin!"
 
