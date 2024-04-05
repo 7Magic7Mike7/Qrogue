38468dc6fdb79eb2ce042b66399f5d1c3cda252a
Michael Artner <michael.artner@hotmail.de>
2024-04-05 22:19:20 +0200

    [SIDE COMMIT] updated transition tests to be unit tests and updated start()'s signature

--- qrogue/test/transition_test.py
+++ qrogue/test/transition_test.py
@@ -1,37 +1,30 @@
-from typing import List
+import unittest
+from typing import List, Optional
 
 from qrogue.graphics.widgets import TransitionWidgetSet
 from qrogue.management import QrogueCUI
-from qrogue.util import Config
+from qrogue.test import test_util
 
 
-class TestQrogueCUI(QrogueCUI):
+class _TestQrogueCUI(QrogueCUI):
     @staticmethod
     def exam_spaceship() -> List[TransitionWidgetSet.TextScroll]:
         return [
             TransitionWidgetSet.TextScroll.hasty("A couple of days later...", text_delay=0.5),
-            TransitionWidgetSet.TextScroll.fast("Mike received a letter stating he was chosen to join Mission Quniverse!",
-                                                clear_previous=True),
+            TransitionWidgetSet.TextScroll.fast("Mike received a letter stating he was chosen to join Mission "
+                                                "Quniverse!", clear_previous=True),
         ]
 
-    def __init__(self, seed: int):
-        super().__init__(seed)
-
-    def start(self):
-        text_scrolls = [
-            TransitionWidgetSet.TextScroll.medium("Welcome to the great", clear_previous=True),
-            TransitionWidgetSet.TextScroll.instant(" \"Mission Quniverse\".", text_delay=0.5),
-            TransitionWidgetSet.TextScroll.hasty("\nWe hope you'll like it - bye!", text_delay=1, clear_previous=True)
-        ]
-        text_scrolls = TestQrogueCUI.exam_spaceship()
+    def start(self, level_name: Optional[str] = None):
+        text_scrolls = self.exam_spaceship()
         self._execute_transition(text_scrolls, QrogueCUI._State.Menu, None, additional_callback=self.stop)
-        super(TestQrogueCUI, self).start()
+        super().start()
 
 
-def transition_test():
-    TestQrogueCUI(7).start()
+class TransitionTests(test_util.SingletonSetupTestCase):
+    def test_transition(self):
+        _TestQrogueCUI(7).start()
 
 
 if __name__ == "__main__":
-    Config.activate_debugging()
-    transition_test()
+    unittest.main()
