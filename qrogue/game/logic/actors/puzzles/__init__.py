113e6d475e19e6c1d13677b16b775c71786d7178
Michael Artner <michael.artner@hotmail.de>
2024-04-26 15:37:43 +0200

    implemented function to check if a puzzle is reachable based on input_stv, target_stv and available gates

--- qrogue/game/logic/actors/puzzles/__init__.py
+++ qrogue/game/logic/actors/puzzles/__init__.py
@@ -5,6 +5,51 @@ from .boss import Boss
 from .riddle import Riddle
 from .challenge import Challenge
 
+import itertools
+from typing import Tuple, Optional, List
+from qrogue.util import QuantumSimulationConfig
+from ...base import CircuitMatrix, StateVector, QuantumCircuit, UnitarySimulator
+from ...collectibles import Instruction
+
+
+def is_puzzle_solvable(input_stv: StateVector, target_stv: StateVector, gate_list: List[Instruction]) \
+        -> Tuple[bool, Optional[QuantumCircuit]]:
+    assert input_stv.num_of_qubits == target_stv.num_of_qubits, "Different number of qubits!"
+
+    simulator = UnitarySimulator()
+    circuit = QuantumCircuit.from_bit_num(input_stv.num_of_qubits, input_stv.num_of_qubits)
+    return __is_reachable_rec(simulator, circuit, input_stv, target_stv, gate_list)
+
+
+def __is_reachable_rec(simulator: UnitarySimulator, circuit: QuantumCircuit, input_stv: StateVector,
+                       target_stv: StateVector, gate_list: List["Instruction"]) \
+        -> Tuple[bool, Optional[QuantumCircuit]]:
+    cur_gate = gate_list.pop()
+    qubits = list(itertools.permutations(range(input_stv.num_of_qubits), cur_gate.num_of_qubits))
+
+    while len(qubits) > 0:
+        cur_circuit = circuit.copy()
+        qargs = qubits.pop()
+        gate = cur_gate.copy().setup(qargs)
+        gate.append_to(cur_circuit)
+
+        amplitudes = simulator.execute(cur_circuit, decimals=QuantumSimulationConfig.DECIMALS)
+        circuit_matrix = CircuitMatrix(amplitudes, 0)  # num of used gates doesn't matter when checking reachability
+        cur_stv = circuit_matrix.multiply(input_stv)
+        diff = cur_stv.get_diff(target_stv)
+
+        if diff.is_zero:
+            return True, cur_circuit
+
+        # go one recursion step deeper and try the next gate
+        if len(gate_list) > 0:
+            ret, winning_circuit = __is_reachable_rec(simulator, cur_circuit, input_stv, target_stv,
+                                                                  gate_list.copy())
+            if ret: return True, winning_circuit
+        # else we try to place cur_gate on different qubits and continue
+
+    return False, None
+
 # importing
 # +util
 # +collectibles
--- qrogue/test/statevector_tests.py
+++ qrogue/test/statevector_tests.py
@@ -1,7 +1,23 @@
 import numpy as np
+import qrogue.game.logic.collectibles.instruction as gates
+from qrogue.game.logic.actors.puzzles import is_puzzle_solvable
 
 from qrogue.game.logic.base import StateVector
+from qrogue.game.logic.collectibles import Instruction
 
 stv = StateVector([1 / np.sqrt(2), 0 + 0j, 0 + 0j, 1 / np.sqrt(2)])
 #stv.extend(1)
-print(stv)
+#print(stv)
+
+
+input_stv_ = StateVector([0, 0, 0, 0, complex(0, -1), 0, 0, 0])
+target_stv_ = Instruction.compute_stv([
+    gates.HGate().setup([1]),
+], 3)
+
+#gate_list_ = [gates.XGate(), gates.CXGate(), gates.HGate(), gates.SGate()]     # this order returns True
+gate_list_ = [gates.HGate(), gates.SGate(), gates.XGate(), gates.CXGate()]      # but this order returns False
+
+ret, circuit = is_puzzle_solvable(input_stv_, target_stv_, gate_list_)
+print(ret)
+print(circuit)
