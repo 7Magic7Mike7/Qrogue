d89918c44b078830e60e21287df3170ed87ad89e
Michael Artner <michael.artner@hotmail.de>
2024-04-28 19:02:50 +0200

    [SIDE COMMIT] forgot to make ExpeditionConfig accessible via .config

--- qrogue/util/config/__init__.py
+++ qrogue/util/config/__init__.py
@@ -5,7 +5,7 @@ from .grammar_config import MapGrammarConfig
 from .py_cui_config import PyCuiConfig, PyCuiColors
 from .visual_config import ColorCode, ColorConfig, PopupConfig, UIConfig, HudConfig, split_text
 from .gameplay_config import GameplayConfig, CheatConfig, MapConfig, PuzzleConfig, ScoreConfig, \
-    QuantumSimulationConfig, ShopConfig, InstructionConfig, Options
+    QuantumSimulationConfig, ShopConfig, InstructionConfig, Options, ExpeditionConfig
 from .error_config import ErrorConfig
 from .config import Config
 
