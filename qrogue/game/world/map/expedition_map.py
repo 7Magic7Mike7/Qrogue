d9fc9936456c4330461067e2bea5f81e2f300b42
Michael Artner <michael.artner@hotmail.de>
2024-06-27 21:05:32 +0200

    [QoL] improved display names of expeditions by hopefully making them cooler/more spacy

--- qrogue/game/world/map/expedition_map.py
+++ qrogue/game/world/map/expedition_map.py
@@ -2,26 +2,37 @@ from typing import List, Callable, Optional
 
 from qrogue.game.logic.actors import Controllable
 from qrogue.game.world.navigation import Coordinate
-from qrogue.util import MapConfig, StvDifficulty
+from qrogue.util import MapConfig, StvDifficulty, RandomManager
 from .map import Map, MapType, Room, MapMetaData
 
 
 class ExpeditionMap(Map):
-    __NAME_CHARS = "".join([f"{chr(c)}{chr(c).lower()}" for c in range(65, 65 + 26)])
+    # chosen characters:
+    #  - Q: because quantum (disregarded for the other criteria)
+    #  - (almost) the same number of consonants (13) and vowels (13)
+    #  - (almost) the same number of lower (12) and upper (14) case letters
+    #  - characters that look cool/spacy appear more often while maintaining the other criteria
+    __NAME_CHARS = "XXxZzBFHTYcvwAaEeIIiUuuOooQQQ"
+
+    @staticmethod
+    def __get_character(value: int, offset: Optional[int] = None) -> str:
+        if offset is not None: value += offset
+        value %= len(ExpeditionMap.__NAME_CHARS)
+        return ExpeditionMap.__NAME_CHARS[value]
 
     @staticmethod
     def to_display_name(difficulty: StvDifficulty, seed: int) -> str:
+        rm = RandomManager.create_new(seed)
         display_name = "Expedition "
         index = 0
-        for diff_level in difficulty.to_code():
-            level = int(diff_level)
-            if index + level >= len(ExpeditionMap.__NAME_CHARS):
-                # restart if we overshoot (depends on the number of existing DifficultyTypes)
-                index -= len(ExpeditionMap.__NAME_CHARS)
-                # indicate the overshoot so expeditions with different difficulties don't accidentally get the same name
-                display_name += "-"
-            display_name += ExpeditionMap.__NAME_CHARS[index + level]
-            index += StvDifficulty.max_difficulty_level()
+        for i, diff_level in enumerate(difficulty.to_code()):
+            # offset makes it so the next character is definitely different
+            offset = rm.get_int(min_=1, max_=len(ExpeditionMap.__NAME_CHARS) - StvDifficulty.max_difficulty_level())
+            character = ExpeditionMap.__get_character(index + int(diff_level), offset)
+            index += StvDifficulty.max_difficulty_level() + offset
+            # the first letter is always upper case
+            if i == 0: display_name += character.upper()
+            else: display_name += character
         return display_name + f"{difficulty.level}*{seed}"
 
     def __init__(self, seed: int, difficulty: StvDifficulty, rooms: List[List[Optional[Room]]],
