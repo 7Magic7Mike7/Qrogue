e7e9b1845e81895690466292efa1549b720e5a05
Michael Artner <michael.artner@hotmail.de>
2024-08-21 19:44:56 +0200

    [Options] can now change option values from config file - previously the config file contained indices instead of values

--- qrogue/util/config/gameplay_config.py
+++ qrogue/util/config/gameplay_config.py
@@ -245,74 +245,91 @@ class CheatConfig:
         return CheatConfig.__use_cheat(code)
 
 
-def _get_boolean_callback() -> Tuple[Callable[[int], str], Callable[[str], bool]]:
-    def get(index: int) -> str:
-        if index % 2 == 1:
-            return "yes"
-        else:
-            return "no"
+class OptionsType(Enum):
+    Bool = 0
+    Float = 1
 
-    def convert(value: str) -> bool:
-        if value.lower() == "yes":
-            return True
-        else:
-            return False
+    def get_callbacks(self, data: Any) -> Tuple[Callable[[int], str], Callable[[str], Any]]:
+        if self is OptionsType.Bool:
+            return OptionsType._get_boolean_callback()
+        elif self is OptionsType.Float:
+            if isinstance(data, Tuple) and len(data) == 3:
+                min_, max_, steps = data
+                return OptionsType._get_float_callback(min_, max_, steps)
+            raise Exception(f"Expected Tuple of length=3 as OptionsType.Float's data but got \"{data}\".")
+        raise Exception("Invalid OptionsType!")
 
-    return get, convert
+    @staticmethod
+    def _get_boolean_callback() -> Tuple[Callable[[int], str], Callable[[str], bool]]:
+        def get(index: int) -> str:
+            if index % 2 == 1:
+                return "yes"
+            else:
+                return "no"
 
+        def convert(value: str) -> bool:
+            if value.lower() == "yes":
+                return True
+            else:
+                return False
 
-def _get_float_callback(min_: float, max_: float, steps: int) -> Tuple[Callable[[int], str], Callable[[str], float]]:
-    assert min_ < max_
-    assert steps > 0
-    range_ = max_ - min_
-    step_size = range_ / steps
+        return get, convert
 
-    def get(index: int) -> str:
-        val = min_ + step_size * index
-        return str(val)
+    @staticmethod
+    def _get_float_callback(min_: float, max_: float, steps: int) -> Tuple[Callable[[int], str], Callable[[str], float]]:
+        assert min_ < max_
+        assert steps > 0
+        range_ = max_ - min_
+        step_size = range_ / steps
 
-    return get, float
+        def get(index: int) -> str:
+            val = min_ + step_size * index
+            return str(val)
+
+        return get, float
 
 
 class Options(Enum):
-    energy_mode = ("Energy Mode", _get_boolean_callback(), 2, 0,
+    energy_mode = ("Energy Mode", OptionsType.Bool, None, 2, 0,
                    "In energy mode every change to your circuit costs energy. You loose if your Robot is out of "
                    "energy.")
 
-    auto_save = ("Auto Save", _get_boolean_callback(), 2, 1,
+    auto_save = ("Auto Save", OptionsType.Bool, None, 2, 1,
                  "Whether to automatically save the game on exit or not.")
-    auto_reset_circuit = ("Auto Reset Circuit", _get_boolean_callback(), 2, 1,
+    auto_reset_circuit = ("Auto Reset Circuit", OptionsType.Bool, None, 2, 1,
                           "Automatically reset your Circuit to a clean state at the beginning of a Puzzle, Riddle, "
                           "etc.")
-    log_keys = ("Log Keys", _get_boolean_callback(), 2, 1,
+    log_keys = ("Log Keys", OptionsType.Bool, None, 2, 1,
                 "Stores all keys you pressed in a .qrkl-file so one can replay them (e.g. for analysing a bug)")
 
-    gameplay_key_pause = ("Gameplay Key Pause", _get_float_callback(0.1, 1.0, 9), 9, 0,
+    gameplay_key_pause = ("Gameplay Key Pause", OptionsType.Float, (0.1, 1.0, 9), 9, 0,
                           "How long to wait before we process the next input during gameplay.")
-    simulation_key_pause = ("Simulation Key Pause", _get_float_callback(0.05, 1.0, 19), 19, 3,
+    simulation_key_pause = ("Simulation Key Pause", OptionsType.Float, (0.05, 1.0, 19), 19, 3,
                             "How long to wait before we process the next input during simulation.")
 
-    show_ket_notation = ("Show Ket-Notation", _get_boolean_callback(), 2, 1,
+    show_ket_notation = ("Show Ket-Notation", OptionsType.Bool, None, 2, 1,
                          "Whether to display ket-notation for state vectors and circuit matrix or not.")
-    allow_implicit_removal = ("Allow implicit Removal", _get_boolean_callback(), 2, 0,
+    allow_implicit_removal = ("Allow implicit Removal", OptionsType.Bool, None, 2, 0,
                               "Allows you to place a gate on an occupied spot, removing the occupying gate in the "
                               "process.")
-    allow_multi_move = ("Allow multi move", _get_boolean_callback(), 2, 1,
+    allow_multi_move = ("Allow multi move", OptionsType.Bool, None, 2, 1,
                         "Allows you to move multiple tiles at once by pressing a number followed by a direction.")
-    auto_skip_text_animation = ("Auto skip text animation", _get_boolean_callback(), 2, 0,
+    auto_skip_text_animation = ("Auto skip text animation", OptionsType.Bool, None, 2, 0,
                                 "During some special scene transitions there will be some animated text describing "
                                 "what happened in-between story sections. Skipping this means that the whole text "
                                 "will be shown at once.")
-    enable_puzzle_history = ("Enable puzzle history", _get_boolean_callback(), 2, 1,
+    enable_puzzle_history = ("Enable puzzle history", OptionsType.Bool, None, 2, 1,
                              "Whether you can navigate through your circuit's history or not while solving a puzzle.")
-    auto_reset_history = ("Auto reset history", _get_boolean_callback(), 2, 1,
+    auto_reset_history = ("Auto reset history", OptionsType.Bool, None, 2, 1,
                           "Whether the puzzle history should automatically jump to the present (= last changes) when "
                           "you navigate through menus.\nNote: Confirming to edit the circuit always resets history.")
 
-    def __init__(self, name: str, get_value: Tuple[Callable[[int], str], Callable[[str], Any]], num_of_values: int,
-                 default_index: int, description: str):
+    def __init__(self, name: str, otype: OptionsType, data: Any, num_of_values: int, default_index: int,
+                 description: str):
         self.__name = name
-        self.__get_value, self.__convert_value = get_value
+        self.__otype = otype
+        self.__data = data
+        self.__get_value, self.__convert_value = otype.get_callbacks(data)
         self.__num_of_values = num_of_values
         self.__default_index = default_index
         self.__description = description
@@ -320,6 +337,14 @@ class Options(Enum):
     @property
     def name(self) -> str:
         return self.__name
+
+    @property
+    def type(self) -> OptionsType:
+        return self.__otype
+
+    @property
+    def data(self) -> Any:
+        return self.__data
 
     @property
     def num_of_values(self) -> int:
@@ -350,6 +375,11 @@ class GameplayConfig:
     __OPTIONS: Dict[Options, int] = {}
 
     __KNOWLEDGE_MODE: _KnowledgeMode = _KnowledgeMode.Newbie
+
+    @staticmethod
+    def validate():
+        # todo: check conversion of value of random index for options?
+        pass
 
     @staticmethod
     def init_options():
@@ -391,9 +421,33 @@ class GameplayConfig:
         text = ""
         for option in GameplayConfig.__OPTIONS:
             cur_index = GameplayConfig.__OPTIONS[option]
-            text += f"{option.name}{GameplayConfig.__KEY_VALUE_SEPARATOR}{cur_index}"
+            text += f"{option.name}{GameplayConfig.__KEY_VALUE_SEPARATOR}{option.convert(cur_index)}"
             text += "\n"
         return text
+
+    @staticmethod
+    def _get_closest_index(option: Options, value: Any) -> int:
+        if option.type is OptionsType.Bool:
+            if isinstance(value, str):
+                return 1 if value.lower().strip() in ["yes", "1"] else 0
+            else:
+                return 1 if value == 1 else 0
+
+        elif option.type is OptionsType.Float:
+            if isinstance(value, str):
+                value = float(value.strip())
+            if isinstance(value, float):
+                min_, max_, steps = option.data
+                if value <= min_: return 0
+                if value >= max_: return steps  # steps is the maximum index
+                # remove the offset of min_ and then check how often step_diff is in the remaining value
+                step_diff = (max_ - min_) / steps
+                return int(round((value - min_) / step_diff))
+            else:
+                # invalid type for value, so we return the default index
+                return option.default_index
+
+        raise NotImplementedError(f"OptionsType {option.type} not implemented!")
 
     @staticmethod
     def from_log_text(log_text: str) -> bool:
@@ -404,11 +458,12 @@ class GameplayConfig:
             if len(line.strip(" ")) == 0:
                 continue
             try:
-                name, index = line.split(GameplayConfig.__KEY_VALUE_SEPARATOR)
+                name, value = line.split(GameplayConfig.__KEY_VALUE_SEPARATOR)
                 name = normalize(name)
-                for val in Options.__members__.values():
-                    if normalize(val.name) == name:  # compare the normalized names
-                        GameplayConfig.__OPTIONS[val] = int(index)
+                for option in Options:
+                    if normalize(option.name) == name:  # compare the normalized names
+                        index = GameplayConfig._get_closest_index(option, value)
+                        GameplayConfig.__OPTIONS[option] = index
                         break
             except IndexError:
                 return False
